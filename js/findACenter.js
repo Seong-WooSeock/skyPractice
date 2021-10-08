31186ecd801c4064b9be0a3a12381aff4bf9eb07
wsseong <woo_seock@naver.com>
2021-10-08 13:12:21 +0900

    4th

--- js/findACenter.js
+++ js/findACenter.js
@@ -1,2 +1,13 @@
-let map = document.getElementById("centerMap");
+let mapCursor = document.getElementById("centerMap")
+let map = document.getElementById("findMapBox");
 
+mapCursor.onclick = () => {
+    map.style.display = "inline";
+}
+map.onmouseover = () => {
+    map.style.display = "inline";
+}
+
+map.onmouseout = () => {
+    map.style.display = "none";
+}
---
+++ js/picChange.js
@@ -0,0 +1,28 @@
+let nick = document.getElementById("nick");
+let zino = document.getElementById("zino");
+let june = document.getElementById("june");
+let erick = document.getElementById("erick");
+let yunee = document.getElementById("yunee");
+
+let pic = document.getElementById("trainerImg");
+
+function nickPic() {
+    pic.src = "../../images/nickmainpic.jpg";
+}
+
+function zinoPic() {
+    pic.src = "../../images/zinomainpic.jpg";
+}
+
+function junePic() {
+    pic.src = "../../images/junemainpic.jpg";
+}
+
+function erickPic() {
+    pic.src = "../../images/erickmainpic.jpg";
+}
+
+function yuneePic() {
+    pic.src = "../../images/yuneemainpic.jpg";
+}
+
