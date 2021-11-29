2507493a883e523919437d4ed44c733f39554fd7
wsseong <woo_seock@naver.com>
2021-11-29 21:17:22 +0900

    회원가입 , 로그인 다크모드구현

--- js/join_idpw_event.js
+++ js/join_idpw_event.js
@@ -125,4 +125,34 @@ repw_input[0].onblur = () => {
 }
 next_btn.addEventListener('click', () => {
     location.href = './join_body.html';
-  })
+})
+  
+// -------------------이미지 경로----------------------
+function lm() {
+    let headerMenuToggleImg = document.getElementById("headerMenuToggleImg");
+    let pilatesLogo = document.getElementById('pilatesLogo');
+    let golfLogo = document.getElementById('golfLogo');
+    let reading_glasses = document.getElementById('reading_glasses');
+    let logoImg = document.getElementById('logoImg');
+
+    pilatesLogo.src = "../../../images/pliatesLogo-white.png";
+    golfLogo.src = "../../../images/golflogo-white.png";
+    reading_glasses.src = "../../../images/findlogo.png";
+    logoImg.src = "../../../images/blacklogo.png";
+    headerMenuToggleImg.src = "../../../images/menuList-black.png";
+}
+
+function dm() {
+    let headerMenuToggleImg = document.getElementById("headerMenuToggleImg");
+    let pilatesLogo = document.getElementById('pilatesLogo');
+    let golfLogo = document.getElementById('golfLogo');
+    let reading_glasses = document.getElementById('reading_glasses');
+    let logoImg = document.getElementById('logoImg');
+    
+    pilatesLogo.src = "../../../images/pliatesLogo.png";
+    golfLogo.src = "../../../images/golflogo.png";
+    reading_glasses.src = "../../../images/findlogo-gold.png";
+    logoImg.src = "../../../images/goldlogo.png";
+    headerMenuToggleImg.src = "../../../images/menuList.png";
+}
+// -------------------이미지 경로----------------------
